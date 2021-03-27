f9e976ce4307b51e2204f559144582bed1fcf150
AGhostik <yakupov.aidar.ru@yandex.ru>
2021-03-27 17:47:39 +0500

    init

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cubeTransform;
+
+    private Quaternion _rotation;
+
+    private void Awake()
+    {
+        _rotation = Quaternion.Euler(0, 0.3f, 0);
+    }
+
+    public async void Fun()
+    {
+        var delayMilliseconds = 3000f;
+        Debug.Log("START!!!");
+
+        var time = Time.time;
+
+        while (delayMilliseconds > 0)
+        {
+            cubeTransform.rotation *= _rotation;
+            var deltatime = await WaitForFrame();
+            delayMilliseconds -= deltatime;
+        }
+        
+        Debug.Log($"END!!! time elapsed - {Time.time - time}");
+    }
+
+    private static async Task<float> WaitForFrame()
+    {
+        var frames = Time.frameCount;
+        
+        while (Time.frameCount == frames)
+            await Task.Yield();
+
+        return Time.deltaTime * 1000;
+    }
+}
